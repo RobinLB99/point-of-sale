060d99630901560cb1cb425bd6e02a5a3bcdb02a
Robin Lugo Boero <rjlb991102@hotmail.com>
2026-04-26 18:04:29 -0500

    refactor: definir interfaz genérica y anotación de control
    - Se crea la interfaz Generic para estandarizar servicios de dominio.
    - Se añade @DisabledOverride para control de infraestructura.

--- pos-system-api/src/main/java/com/robinlugoboero/possystemapi/domain/service/interfaces/Generic.java
+++ pos-system-api/src/main/java/com/robinlugoboero/possystemapi/domain/service/interfaces/Generic.java
@@ -2,6 +2,9 @@ package com.robinlugoboero.possystemapi.domain.service.interfaces;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface Generic<E> {
 	
 	public E save(E entity);
@@ -11,4 +14,7 @@ public interface Generic<E> {
 	public E find(Long id);
 	
 	public List<E> findAll();
+	
+	public Page<E> getPaginated(Pageable pageable);
+	
 }
